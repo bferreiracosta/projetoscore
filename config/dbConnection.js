34b0cd64c8e84001fce3e840bae13b8bb8d655bd
bferreira <bferreiracosta@uol.com.br>
2021-04-13 09:29:40 -0300

    banco

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,22 +1,22 @@
 var mysql = require('mysql');
 
-const pool = mysql.createPool({
-    connectionLimit: 25,
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'portal_pacientes',
-
-});
-
 // const pool = mysql.createPool({
 //     connectionLimit: 25,
-//     host : 'portal-paciente.mysql.uhserver.com',
-//     user : 'portal_paciente',
-//     password : 'Bflc1989@',
-//     database : 'portal_paciente',
+//     host : 'localhost',
+//     user : 'root',
+//     password : '',
+//     database : 'portal_pacientes',
 
 // });
+
+const pool = mysql.createPool({
+    connectionLimit: 25,
+    host : 'portal-paciente.mysql.uhserver.com',
+    user : 'portal_paciente',
+    password : 'Bflc1989@',
+    database : 'portal_paciente',
+
+});
 
 
 
