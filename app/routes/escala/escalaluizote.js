b2609ade2a557df39809e0ebe7c2a70a8d9e77d7
Bruno <bferreiracosta@uol.com.br>
2021-09-10 14:04:50 -0300

    correcao escala

--- app/routes/escala/escalaluizote.js
+++ app/routes/escala/escalaluizote.js
@@ -7,7 +7,7 @@ module.exports = function(application){
 		});
 	
 	application.get('/burcarferiado', function(req, res){
-		fs.readFile('/home/bruno/Área de Trabalho/Sites/ProjetoSCIH - Original/app/routes/escala/Feriados', (err, resultado) => {
+		fs.readFile('/app/routes/escala/Feriados', (err, resultado) => {
 			if (err) throw err;
 
 			res.send(resultado);
