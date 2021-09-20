e0e84867ddc5a707ddb4d788aa76d32a77daca7f
Bruno <bferreiracosta@uol.com.br>
2021-09-20 08:13:36 -0300

    troca de modulos

--- app/model/admin/modeladmin.js
+++ app/model/admin/modeladmin.js
@@ -266,16 +266,191 @@ modeladmin.prototype.trocarmodulo = function(usuario, senha, funcao, req, res){
 	var senha = senha;
 	var funcao = funcao;
 
-			if(funcao == 'SCIH'){
+	if(funcao == 'SCIH'){
+		if (usuario && senha) {
+			this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+				
+					if (results.length > 0) {
+
+						if(results[0].scih == 1){
+						req.session.loggedin = true;
+						req.session.usuario = results[0].usuario;
+						res.render('home/home', {id : results});
+					} else {
+						var mensage = "Você nao tem autorização para esse modulo";
+						res.render("home/index", {msg : mensage});
+					}			
+				}
+				else{
+					var mensage = "Usuario o senha incorreto";
+						res.render("home/index", {msg : mensage});
+				}
+				res.end();
+			});
+		} else {
+			var mensage = "Por favor entre com usuario e senha";
+						res.render("home/index", {msg : mensage});
+		}
+	}else if(funcao == 'SM Hospitalidade') {
+			if (usuario && senha) {
+				this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+					
+						if (results.length > 0) {
+							if(results[0].smh == 1){
+							req.session.loggedin = true;
+							req.session.usuario = results[0].usuario;
+							res.render('home/homemental', {id : results});
+						} else {
+							var mensage = "Você nao tem autorização para esse modulo";
+							res.render("home/index", {msg : mensage});
+						}			
+					}
+					else{
+						var mensage = "Usuario o senha incorreto";
+							res.render("home/index", {msg : mensage});
+					}
+					res.end();
+				});
+			} else {
+				var mensage = "Por favor entre com usuario e senha";
+							res.render("home/index", {msg : mensage});
+			}
+		}else if(funcao == 'SM Urgência') {
+			if (usuario && senha) {
+				this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+					
+						if (results.length > 0) {
+							if(results[0].smu == 1){
+							req.session.loggedin = true;
+							req.session.usuario = results[0].usuario;
+							
+							res.render('home/homementalurgencia', {id : results});
+						} else {
+							var mensage = "Você nao tem autorização para esse modulo";
+							res.render("home/index", {msg : mensage});
+						}			
+					}
+					else{
+						var mensage = "Usuario o senha incorreto";
+							res.render("home/index", {msg : mensage});
+					}
+					res.end();
+				});
+			} else {
+				var mensage = "Por favor entre com usuario e senha";
+							res.render("home/index", {msg : mensage});
+			}
+		}else if(funcao == 'Gestão de Leitos') {
+			if (usuario && senha) {
+				this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+					
+						if (results.length > 0) {
+							if(results[0].gestao == 1){
+							req.session.loggedin = true;
+							req.session.usuario = results[0].usuario;
+							
+							res.render('home/homegestao', {id : results});
+						} else {
+							var mensage = "Você nao tem autorização para esse modulo";
+							res.render("home/index", {msg : mensage});
+						}			
+					}
+					else{
+						var mensage = "Usuario o senha incorreto";
+							res.render("home/index", {msg : mensage});
+					}
+					res.end();
+				});
+			} else {
+				var mensage = "Por favor entre com usuario e senha";
+							res.render("home/index", {msg : mensage});
+			}
+		}else if(funcao == 'Central de Equipamentos') {
+			if (usuario && senha) {
+				this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+					
+						if (results.length > 0) {
+							if(results[0].equipamentos == 1){
+							req.session.loggedin = true;
+							req.session.usuario = results[0].usuario;
+							
+							res.render('home/homeequipamentos', {id : results});
+						} else {
+							var mensage = "Você nao tem autorização para esse modulo";
+							res.render("home/index", {msg : mensage});
+						}			
+					}
+					else{
+						var mensage = "Usuario o senha incorreto";
+							res.render("home/index", {msg : mensage});
+					}
+					res.end();
+				});
+			} else {
+				var mensage = "Por favor entre com usuario e senha";
+							res.render("home/index", {msg : mensage});
+			}
+		}else if(funcao == 'Obstetricia') {
+			if (usuario && senha) {
+				this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+					
+						if (results.length > 0) {
+							if(results[0].obstetricia == 1 || results[0].aps == 1){
+							req.session.loggedin = true;
+							req.session.usuario = results[0].usuario;
+							
+							res.render('home/homeobstetricia', {id : results});
+						} else {
+							var mensage = "Você nao tem autorização para esse modulo";
+							res.render("home/index", {msg : mensage});
+						}			
+					}
+					else{
+						var mensage = "Usuario o senha incorreto";
+							res.render("home/index", {msg : mensage});
+					}
+					res.end();
+				});
+			} else {
+				var mensage = "Por favor entre com usuario e senha";
+							res.render("home/index", {msg : mensage});
+			}
+		}else if(funcao == 'Kamban') {
+			if (usuario && senha) {
+				this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+					
+						if (results.length > 0) {
+							if(results[0].kamban == 1){
+							req.session.loggedin = true;
+							req.session.usuario = results[0].usuario;
+														
+							res.render('home/homekaban', {id : results});
+						} else {
+							var mensage = "Você nao tem autorização para esse modulo";
+							res.render("home/index", {msg : mensage});
+						}			
+					}
+					else{
+						var mensage = "Usuario o senha incorreto";
+							res.render("home/index", {msg : mensage});
+					}
+					res.end();
+				});
+			} else {
+				var mensage = "Por favor entre com usuario e senha";
+							res.render("home/index", {msg : mensage});
+			}
+		}else if(funcao == 'Escala') {
+			
 				if (usuario && senha) {
 					this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
 						
 							if (results.length > 0) {
-	
-								if(results[0].scih == 1){
+								if(results[0].escala == 1){
 								req.session.loggedin = true;
 								req.session.usuario = results[0].usuario;
-								res.render('home/home', {id : results});
+															
+								res.render('home/homeescala', {id : results});
 							} else {
 								var mensage = "Você nao tem autorização para esse modulo";
 								res.render("home/index", {msg : mensage});
@@ -291,185 +466,36 @@ modeladmin.prototype.trocarmodulo = function(usuario, senha, funcao, req, res){
 					var mensage = "Por favor entre com usuario e senha";
 								res.render("home/index", {msg : mensage});
 				}
-			}else if(funcao == 'SM Hospitalidade') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].smh == 1){
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-									res.render('home/homemental', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
-				}else if(funcao == 'SM Urgência') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].smu == 1){
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-									
-									res.render('home/homementalurgencia', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
-				}else if(funcao == 'Gestão de Leitos') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].gestao == 1){
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-									
-									res.render('home/homegestao', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
-				}else if(funcao == 'Central de Equipamentos') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].equipamentos == 1){
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-									
-									res.render('home/homeequipamentos', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
-				}else if(funcao == 'Obstetricia') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].obstetricia == 1 || results[0].aps == 1){
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-									
-									res.render('home/homeobstetricia', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
-				}else if(funcao == 'Kamban') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].kamban == 1){
-										
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-																
-									res.render('home/homekaban', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
-				}else if(funcao == 'administrativo') {
-					if (usuario && senha) {
-						this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
-							
-								if (results.length > 0) {
-									if(results[0].admin == 1){
-									req.session.loggedin = true;
-									req.session.usuario = results[0].usuario;
-																
-									res.render('home/homeadministrativo', {id : results});
-								} else {
-									var mensage = "Você nao tem autorização para esse modulo";
-									res.render("home/index", {msg : mensage});
-								}			
-							}
-							else{
-								var mensage = "Usuario o senha incorreto";
-									res.render("home/index", {msg : mensage});
-							}
-							res.end();
-						});
-					} else {
-						var mensage = "Por favor entre com usuario e senha";
-									res.render("home/index", {msg : mensage});
-					}
+			}else if(funcao == 'Escala Atribuição') {
+			
+				if (usuario && senha) {
+					this._conection.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [usuario, senha], function(error, results, fields) {
+						
+							if (results.length > 0) {
+								if(results[0].atribuicao == 1){
+								req.session.loggedin = true;
+								req.session.usuario = results[0].usuario;
+															
+								res.render('home/homeatribuicao', {id : results});
+							} else {
+								var mensage = "Você nao tem autorização para esse modulo";
+								res.render("home/index", {msg : mensage});
+							}			
+						}
+						else{
+							var mensage = "Usuario o senha incorreto";
+								res.render("home/index", {msg : mensage});
+						}
+						res.end();
+					});
 				} else {
-		var mensage = "Por favor entre com usuario e senha";
-					res.render("home/index", {msg : mensage});
-	}			
+					var mensage = "Por favor entre com usuario e senha";
+								res.render("home/index", {msg : mensage});
+				}
+		} else {
+var mensage = "Por favor entre com usuario e senha";
+			res.render("home/index", {msg : mensage});
+}			
 }
 
 modeladmin.prototype.updatepassword = function(usuario, senha,  callback){
