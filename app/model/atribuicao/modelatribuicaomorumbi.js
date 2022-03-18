60aceff747e7102205b51f99219acf1ca40d7694
bferreiracosta <bferreiracosta@uol.com.br>
2022-03-18 19:40:23 -0300

    correcao atribuicao morumbi

--- app/model/atribuicao/modelatribuicaomorumbi.js
+++ app/model/atribuicao/modelatribuicaomorumbi.js
@@ -17,9 +17,9 @@ modelatribuicaomorumbi.prototype.updateescalamorumbi = function(unidade, date, t
 	this._conection.query('update atribuicaomorumbi set enf1="'+enf1+'", tecenf1="'+tecenf1+'", maq1="'+maq1+'", maq2="'+maq2+'", enfpreposto="'+enfpreposto+'", tecenf2="'+tecenf2+'", tecenf3="'+tecenf3+'", tecenf4="'+tecenf4+'", tecenf5="'+tecenf5+'",tecenf6="'+tecenf6+'", tecenf7="'+tecenf7+'", tecenf8="'+tecenf8+'", tecenf9="'+tecenf9+'", tecenf10="'+tecenf10+'", tecenf11="'+tecenf11+'", tecenf12="'+tecenf12+'", tecenf13="'+tecenf13+'",enf2="'+enf2+'", enf3="'+enf3+'", enf4="'+enf4+'", enf5="'+enf5+'", teccme="'+teccme+'" , enfcme="'+enfcme+'" where unidade="Morumbi" and date="'+date+'"and turno="'+turno+'"', callback);
 }
 
-modelatribuicaomorumbi.prototype.adicionandoescalafolga = function(unidade, date, turno, enfermeiro1, enfermeiro2, enfermeiro3, enfermeiro4, tecnico1, tecnico2, tecnico3, tecnico4, atestado1,atestado2, atestado3, atestado4, callback){
+modelatribuicaomorumbi.prototype.adicionandoescalafolga = function(unidade, date, turno, enfermeiro1, enfermeiro2, enfermeiro3, tecnico1, tecnico2, tecnico3, atestado1,atestado2, atestado3, callback){
 	console.log(unidade, date, turno)
-	this._conection.query('update atribuicaomorumbi set enfermeiro1="'+enfermeiro1+'", enfermeiro2="'+enfermeiro2+'", enfermeiro3="'+enfermeiro3+'", enfermeiro4="'+enfermeiro4+'", tecnico1="'+tecnico1+'", tecnico2="'+tecnico2+'", tecnico3="'+tecnico3+'", tecnico4="'+tecnico4+'", atestado1="'+atestado1+'",atestado2="'+atestado2+'", atestado3="'+atestado3+'", atestado4="'+atestado4+'" where unidade="Morumbi" and date="'+date+'"and turno="'+turno+'"', callback);
+	this._conection.query('update atribuicaomorumbi set enfermeiro1="'+enfermeiro1+'", enfermeiro2="'+enfermeiro2+'", enfermeiro3="'+enfermeiro3+'", tecnico1="'+tecnico1+'", tecnico2="'+tecnico2+'", tecnico3="'+tecnico3+'", atestado1="'+atestado1+'",atestado2="'+atestado2+'", atestado3="'+atestado3+'" where unidade="Morumbi" and date="'+date+'"and turno="'+turno+'"', callback);
 }
 
 modelatribuicaomorumbi.prototype.updateescalaparadamorumbi = function(unidade, date, turno, monitorizacao1, monitorizacao2, anotacao1, anotacao2, compressao1, compressao2, ventilacao1, ventilacao2, medicacao1, medicacao2, callback){
